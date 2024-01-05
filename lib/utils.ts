421cc76a25ec9e20626357d820c3fa9820e64f4b
Manu Kapoor <manu.web3.0@gmail.com>
2024-01-04 22:18:57 -0700

    Added Organizational routes, middleware, sign-up/in/out, light/dark themes

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// an imp. util/helper to create dynamic CSS classes - avoids naming conflicts by itself
